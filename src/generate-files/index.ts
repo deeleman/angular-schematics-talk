deb58b5402a21b3ad5684ef7d8b5725f154e95c3
Natalia Venditto <natalia.venditto@netcentric.biz>
2018-12-20 14:07:09 +0100

    ISSUE-2 extend logic to create global and _core partial

--- src/generate-files/index.ts
+++ src/generate-files/index.ts
@@ -5,17 +5,32 @@ import { Rule,
   contentTemplate,
   url,
   mergeWith,
+  branchAndMerge,
   chain,
-  move } from '@angular-devkit/schematics';
+  move,
+  noop,
+  filter } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+
 
 export function generateFiles(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    _context.logger.info(JSON.stringify(options));
+    // we need to define a source, which conventionally is the files folder inside of the schematics folder
     const source = apply(url('./files'), [
+      // do not regeneate core and global unless flag is there
+      options.core ? noop() : filter(path => !path.includes('core')),
+      options.global ? noop() : filter(path => !path.includes('global')),
       contentTemplate({
-        name: options.name
-      }),
+        // option `flat` if true, creates the file/directory at top level,
+        // otherwise skips generation
+        'isFlat': (s: string) => (options.flat ? '' : s),
+        ...strings,
+        ...(options as object)
+      } as any),
+      // since create alwats creates at top level, we then move it under the right path
       move(options.path),
     ]);
-    return chain([mergeWith(source)])(tree, _context);
+    return chain([branchAndMerge(chain([mergeWith(source)]))])(tree, _context);
   };
 }
